3824959cafbfbbc885a178bb4e889d7eff8b6996
molefrog <molefrog@gmail.com>
2013-09-19 10:41:52 +0400

    Added motion easing; Project structure;

---
+++ demo/mouse/js/stub.js
@@ -0,0 +1,45 @@
+	$(function() {
+	var imagePath = "../resources/3.jpg";
+	var mp3Path   = "../resources/2.mp3";
+
+	var container = $("#container");
+
+	resizeHandler = function() {
+		container.height( $(window).height() );
+		container.width( $(window).width() );
+	};
+
+    var kaleidoscope = new Kaleidoscope( container[0] );
+
+
+    // Init Drag'n'Drop 
+    var dragdrop = new DragDrop(container[0], /^image/i, function (result) {
+    	var img = new Image();
+        img.src = result;
+        kaleidoscope.image = img;
+    });
+       
+    setInterval(function() {
+    	kaleidoscope.draw();
+    }, 1000 / 30);
+
+    var image = new Image();
+    image.src = imagePath;
+    image.onload = function() {
+        kaleidoscope.image = image;
+    };
+
+    $(window).mousemove(function(event) {
+		var factorx = event.pageX / $(window).width();
+		var factory = event.pageY / $(window).height();
+
+
+		kaleidoscope.angleTarget = factorx;
+		kaleidoscope.zoomTarget  = 1.0 + 1.5 * factory;
+    });
+
+	$(window).resize(resizeHandler);
+	$(window).resize();
+
+
+});
